4c2018953ca7ac7f08c0daba759a98e6bc003b4a
Sonia Hernandez Fierros <hernandezfierrossonia.2023.1@gmail.com>
2023-03-06 18:01:20 +0000

    apuntes

--- Laboratorio_2/codigo8.c
+++ Laboratorio_2/codigo8.c
@@ -1,18 +1,21 @@
  #include<stdio.h>
- /*
-Se recorre un arreglo bidimensional a través de un apuntador
- */
+/*
+    Se recorre un arreglo bidimensional a través de un apuntador
+*/
 
 int main()
 {
+    int nums[3][3]={{99,88,77}, {66,55,44}, {33,22,11}};
     int *ap, indice;
-    int nums[3][3] = {{99,88,77}, {66,55,44}, {33,22,11}};
-    ap = nums;
-    for (indice = 0; indice < 9 ; indice++)
+    ap = nums;//ap = &nums[0];//ap=&nums
+    //dezconozco por qué el warning
+
+    for(indice=0; indice<9 ; indice++)
     {
-        if ((indice%3)==0)
-            printf("\n");
-        printf("%x\t",(ap+indice));
+        if((indice%3)==0)
+            printf("\n");//muy tonto el código, solo los primeros cumplen, inútil en mi opinión
+            //cuando una cadena se repita varias veces puedes convertirla a raw string, creo
+        printf("%p\t",(ap+indice));//imprime dirección?
     }
     return 0;
 }
