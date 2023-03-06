d31562a18f2b2b0c4c5671a9d5b65eb4ac7aab8f
Sonia Hernandez Fierros <hernandezfierrossonia.2023.1@gmail.com>
2023-03-06 16:15:44 +0000

    comprender

--- Laboratorio_2/codigo2.c
+++ Laboratorio_2/codigo2.c
@@ -4,15 +4,21 @@
 */
 int main () 
 {
-    char *ap, c;//declaraciòn de una variable y un char
+    //char *ap, c;//declaraciòn de una variable y un char
+    char c;
+    char *ap;
     c = 'x'; 
     ap = &c;
 
-    // imprime el carácter de la localidad a la que apunta
-    printf("Carácter: %c\n",*ap);//contenido original
-    // imprime el código ASCII de la localidad a la que apunta 
-    printf("Código ASCII: %d\n",*ap);
-    // imprime la dirección de memoria de la localidad a la que apunta 
-    printf("Dirección de memoria: %p\n",ap);
+    //imprime el carácter de la localidad a la que apunta
+    printf("Carácter: %c\n",*ap);//contenido original//contenido original de la localidad a la que apunta
+
+    //imprime el código ASCII de la localidad a la que apunta 
+    printf("Código ASCII: %d\n",*ap);//imprime 120
+    printf("Prueba Código ASCII: %d\n", c);//imprime 20
+
+    //imprime la dirección de memoria de la localidad a la que apunta 
+    printf("Dirección de memoria: %p\n",ap);//dirección original
+
     return 0; 
 }
