822952feb62cd1d14c1088826e607e6fb824f550
Sonia Hernandez Fierros <hernandezfierrossonia.2023.1@gmail.com>
2023-03-17 01:34:03 +0000

    terminada

--- Practica_03/codigo.c
+++ Practica_03/codigo.c
@@ -1,61 +1,58 @@
 #include <stdio.h>
 
-/*Mira, no puedes elegir la de arreglos, a menos que me quede más claro para usar solo un arreglo.
-Puedes crear un estruct en el main(uno para cada pieza) con la asignación de una función a donde envías los datos, incluyendo las coordenadas, y así 
-la función es del mismo tipo estruct*/
+struct board 
+{
+    char tablita[8][8];
+};
 
 struct bishop
 {
     int x;
     int y;
-    //char tablero[8][8];//se me hace muy feo crear un arreglo por cada struct, es más se van a crear muchisismos, aunque no sé explicar por qué
-
-};// caballo
+    struct board tab;
+}piecezota;// caballo
 
 struct pawn //alfil
 {
     int x;
     int y;
-    //char tablero[8][8];
-    
-};//pieza;
+    char tablero[8][8];
+}pieza;
 
 struct rook //torre
 {
     int x;
     int y;
-    //char tablero[8][8];
-
-};
+    char tablero[8][8];
+}piececita;
 
 struct queen
 {
     int x;
     int y;
-    //char tablero[8][8];
-
-};
+    char tablero[8][8];
+}arreglo[1];
 
 struct king
 {
     int x;
     int y;
-    //char tablero[8][8];
-};
+    char tablero[8][8];
+}piece;
 
 void caballo(struct bishop* );
-void alfil(struct pawn *);
-void torre(struct rook* );
+struct pawn alfil();
+void torre();
 void reina(struct queen*);
-void rey(struct king* );
+void rey(int , int , char (*)[8]);
 
 int main()
 {
     int opcion;
-
-    for(;;)//while(1) while(true)
+                    int i,j;
+    for(;;)
     {
-        printf("Elige una opción:\n");//no podemos crear un arreglo de las piezas por qu eno serían del mismo tipo(struct)
+        printf("Elige una opción:\n");
         printf("1) Caballo\n");
         printf("2) ALfil\n");
         printf("3) Torre\n");
@@ -67,24 +64,33 @@ int main()
         switch(opcion)
         {
             case 1:
-                struct bishop arreglo[1];//una pieza, solo quería ser excentrica
-                caballo(arreglo);//estas enviando una dirección
+                caballo(&piecezota);
                 break;
             case 2:
-                struct pawn pieza;
-                alfil(&pieza);
+                pieza=alfil();
+                for(i=0;i<8;i++)
+                {
+                    for(j=0;j<8;j++)
+                        printf("%c", pieza.tablero[i][j]);
+                    printf("\n");
+                }
+                printf("\n");
                 break;
             case 3:
-                struct rook piececita;//luego probar si puedo poner igual pieza
-                torre(&piececita);
+                torre();
                 break;
             case 4:
-                struct queen* piecezota[1];
-                reina(piecezota);
+                reina(arreglo);
                 break;
             case 5:
-                struct king piece;
-                rey(&piece);
+                printf("Escribe las coordenadas (desde 1 a 8):\n");
+                printf("Ingresa la coordenada x: ");
+                scanf("%i", &piece.x);
+                piece.x-=1;
+                printf("Ingresa la coordenada y: ");
+                scanf("%i", &piece.y);
+                piece.y-=1;
+                rey(piece.x, piece.y, &piece.tablero[0][0]);
                 break;
             case 6:
                 return 18;
@@ -92,86 +98,268 @@ int main()
             default:
                 printf("Opción inválida\n");
         }
-
     }
 }
 
-void caballo(struct bishop arreglo[1])//me pregunto que pasa si no lo recibo como un arreglo de uno y solo como una variable, quién sabe, por que en la practica 2 no sabía eso y recibí la dirección del arreglo muchas veces
-{//sinónimo de alfil
-    arreglo[1].x=3;
-    arreglo[1].y=7;
-    /*arreglo[1].tablero={
-        {'o','o','o','o','o','o','o','o'},
-        {'o','o','o','o','o','o','o','o'}, 
-        {'o','o','o','o','o','o','C','o'},    
-        {'o','o','o','o','o','o','o','o'},    
-        {'o','o','o','o','o','o','o','o'},    
-        {'o','o','o','o','o','o','o','o'},    
-        {'o','o','o','o','o','o','o','o'},    
-        {'o','o','o','o','o','o','o','o'},    
-    };*/
+void caballo(struct bishop* jinete)
+{
+    int i,j;
+
+    printf("Escribe las coordenadas (desde 1 a 8):\n");
+    printf("Ingresa la coordenada x: ");
+    scanf("%i", &jinete->x);
+    printf("Ingresa la coordenada y: ");
+    scanf("%i", &jinete->y);
+    (jinete->x)-=1;
+    (jinete->y)-=1;
+    
+    for(i=0; i<=7; ++i)
+        for(j=0; j<=7; ++j)
+            jinete->tab.tablita[i][j]='o';
+
+    i=(jinete->x);
+    j=(jinete->y);
+
+    jinete->tab.tablita[i][j]='C';
+
+    if((i-2)>=0)
+    {
+        if((j-1)>=0)
+            jinete->tab.tablita[i-2][j-1]='x';
+        if((j+1)<=7)
+            jinete->tab.tablita[i-2][j+1]='x';
+    }
+    if((i+2)<=7)
+    {
+        if((j-1)>=0)
+            jinete->tab.tablita[i+2][j-1]='x';
+        if((j+1)<=7)
+            jinete->tab.tablita[i+2][j+1]='x';
+    }
+    if((j-2)>=0)
+    {
+        if((i-1)>=0)
+            jinete->tab.tablita[i-1][j-2]='x';
+        if((i+1)<=7)
+            jinete->tab.tablita[i+1][j-2]='x';
+    }
+    if((j+2)<=7)
+    {
+        if((i-1)>=0)
+            jinete->tab.tablita[i-1][j+2]='x';
+        if((i+1)<=7)
+            jinete->tab.tablita[i+1][j+2]='x';
+    }
+    printf("\n\n");
+    for(i=0; i<=7; ++i)
+    {
+        for(j=0; j<=7; ++j)
+            printf("%c", jinete->tab.tablita[i][j]);
+        printf("\n");
+    }
 }
 
-void alfil(struct pawn* obispo)//recibimos direcciones
+struct pawn alfil()
 {
-    obispo->x=2;
-    obispo->y=8;
-    /*obispo.tablero={
-        {'o','o','o','o','o','o','o','o'},
-        {'o','o','o','o','o','o','o','A'}, 
-        {'o','o','o','o','o','o','o','o'},    
-        {'o','o','o','o','o','o','o','o'},    
-        {'o','o','o','o','o','o','o','o'},    
-        {'o','o','o','o','o','o','o','o'},    
-        {'o','o','o','o','o','o','o','o'},    
-        {'o','o','o','o','o','o','o','o'},    
-    };*/
+    struct pawn obispo;
+
+    int i, j, z;
+
+    printf("\n");
+    printf("Escribe las coordenadas (desde 1 a 8):\n");
+    printf("Coordenada x: ");
+    scanf("%i", &obispo.x);
+    printf("Coordenada y: ");
+    scanf("%i", &obispo.y);
+    printf("\n");
+                
+    obispo.x=obispo.x-1;
+    obispo.y=obispo.y-1;
+
+    for(i=0;i<8;i++)
+        for(j=0;j<8;j++)
+            obispo.tablero[i][j]='o';
+
+    obispo.tablero[obispo.x][obispo.y]='A';
+
+    //SUPERIORES
+    i=obispo.x;
+    j=z=obispo.y;
+    while(i>0)
+    {
+        i-=1;
+        //superior izquierda
+        if(j>0)
+            obispo.tablero[i][j-=1]='x';
+        //superior derecha
+        if(z<7)
+            obispo.tablero[i][z+=1]='x';
+    }
+
+  //INFERIORES
+    i=obispo.x;
+    j=z=obispo.y;
+    while(i<7)
+    {
+        i+=1;
+        //inferior izquierda
+        if((j>0))
+            obispo.tablero[i][j-=1]='x';
+        //inferior derecha
+        if(z<7)
+            obispo.tablero[i][z+=1]='x';
+    }        
+    return obispo;
 }
 
-void torre(struct rook* roque)//torre antiguamente conocida como roque
-{
-    roque->x=1;
-    roque->y=3;
-    /*roque.tablero={
-        {'o','o','T','o','o','o','o','o'},
-        {'o','o','o','o','o','o','o','o'}, 
-        {'o','o','o','o','o','o','o','o'},    
-        {'o','o','o','o','o','o','o','o'},    
-        {'o','o','o','o','o','o','o','o'},    
-        {'o','o','o','o','o','o','o','o'},    
-        {'o','o','o','o','o','o','o','o'},    
-        {'o','o','o','o','o','o','o','o'},    
-    };*/
+void torre()
+{    
+    int i, j;
+
+    printf("\n");
+    printf("Escribe las coordenadas (desde 1 a 8):\n");
+    printf("Coordenada x: ");
+    scanf("%i", &piececita.x);
+    printf("Coordenada y: ");
+    scanf("%i", &piececita.y);
+    printf("\n");
+
+    piececita.x-=1;
+    piececita.y-=1;
+
+    for(i=0;i<8;i++)
+        for(j=0;j<8;j++)
+            piececita.tablero[i][j]='o';
+
+    piececita.tablero[piececita.x][piececita.y]='T';
+
+    for(i=0;i<8;i++)
+        for(j=0;j<8;j++)
+            if((i!=piececita.x)||(j!=piececita.y))//no entra si es la coordenada que nos dió el usuario
+                if((i==piececita.x)||(j==piececita.y))
+                    piececita.tablero[i][j]='x';
+                
+    for(i=0;i<8;i++)
+    {
+        for(j=0;j<8;j++)
+            printf("%c", piececita.tablero[i][j]);
+        printf("\n");
+    } 
+    printf("\n");
 }
 
-void reina(struct queen array[1])//creo que los arreglos son los únicos a quienes cuando los declaras hasta arriba como apuntadores, aquí ya no hace falta
+
+void reina(struct queen array[1])
 {
-    array[1].x=5;
-    array[1].y=2;
-    /*array[1].tablero={
-        {'o','o','o','o','o','o','o','o'},
-        {'o','o','o','o','o','o','o','o'}, 
-        {'o','o','o','o','o','o','C','o'},    
-        {'o','o','o','o','o','o','o','o'},    
-        {'o','Q','o','o','o','o','o','o'},    
-        {'o','o','o','o','o','o','o','o'},    
-        {'o','o','o','o','o','o','o','o'},    
-        {'o','o','o','o','o','o','o','o'},    
-    };*/
+    int i, j, z;
+
+    printf("\n");
+    printf("Escribe las coordenadas (desde 1 a 8):\n");
+    printf("Coordenada x: ");
+    scanf("%i", &array[0].x);//% es el tipo del último valor 
+    printf("Coordenada y: ");
+    scanf("%i", &array[0].y);
+    printf("\n");
+
+    array[0].x-=1;
+    array[0].y-=1;
+
+    for(i=0;i<8;i++)
+        for(j=0;j<8;j++)
+            array[0].tablero[i][j]='o';
+    array[0].tablero[array[0].x][array[0].y]='Q';
+
+    //SUPERIORES
+    i=array[0].x;
+    j=z=array[0].y;
+    while(i>0)
+    {
+        i-=1;
+        //superior izquierda
+        if(j>0)
+            array[0].tablero[i][j-=1]='x';
+        //superior derecha
+        if(z<7)
+            array[0].tablero[i][z+=1]='x';
+    }
+
+  //INFERIORES
+    i=array[0].x;
+    j=z=array[0].y;
+    while(i<7)
+    {
+        i+=1;
+        //inferior izquierda
+        if((j>0))
+            array[0].tablero[i][j-=1]='x';
+        //inferior derecha
+        if(z<7)
+            array[0].tablero[i][z+=1]='x';
+    }
+
+    //Verticales y horizontales
+    for(i=0;i<=7;i++)
+        for(j=0;j<=7;j++)
+            if((i!=array[0].x)||(j!=array[0].y))//no entra si es la coordenada que nos dió el usuario
+                if((i==array[0].x)||(j==array[0].y))
+                    array[0].tablero[i][j]='x';
+
+    for(i=0;i<8;i++)
+    {
+        for(j=0;j<8;j++)
+            printf("%c", array[0].tablero[i][j]);
+        printf("\n");
+    }
+    printf("\n");
 }
 
-void rey(struct king* monarca)
+void rey(int x, int y, char (*monarca)[8])
 {
-    monarca->x=1;
-    monarca->y=4;
-    /*monarca.tablero={
-        {'o','o','o','R','o','o','o','o'},
-        {'o','o','o','o','o','o','o','o'}, 
-        {'o','o','o','o','o','o','o','o'},    
-        {'o','o','o','o','o','o','o','o'},    
-        {'o','o','o','o','o','o','o','o'},    
-        {'o','o','o','o','o','o','o','o'},    
-        {'o','o','o','o','o','o','o','o'},    
-        {'o','o','o','o','o','o','o','o'},    
-    };*/
+    int i, j;
+
+    for(i=0;i<8;i++)
+        for(j=0;j<8;j++)
+            monarca[i][j]='o';
+    monarca[x][y]='R';
+
+    //superiores
+    if(x>0)
+    {
+        //superior izquierda
+        if(y>0)
+            monarca[x-1][y-1]='x';
+        //superior derecha
+        if(y<7)
+            monarca[x-1][y+1]='x';
+    }
+    
+    //inferiores
+    if(x<7)
+    {
+        //inferior izquierda
+        if(y>0) 
+            monarca[x+1][y-1]='x';
+        //inferior derecha
+        if(y<7)
+            monarca[x+1][y+1]='x';
+    }
+
+    //Horizontal y vertical
+    if(y<7)//derecha
+        monarca[x][y+1]='x';
+    if(y>0)//izquierda
+        monarca[x][y-1]='x';//x no se modifica 
+    if(x<7)//abajo
+        monarca[x+1][y]='x';
+    if(x>0)//arriba
+        monarca[x-1][y]='x';
+
+    printf("\n");
+    for(i=0;i<8;i++)
+    {
+        for(j=0;j<8;j++)
+            printf("%c", monarca[i][j]);
+        printf("\n");
+    }
+    printf("\n");
 }
