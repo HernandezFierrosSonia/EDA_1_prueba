ed04a1263d27dae21a7fa74591b7d7ad743b7b4b
Sonia Hernandez Fierros <hernandezfierrossonia.2023.1@gmail.com>
2023-03-29 04:59:34 +0000

    parcial1

--- Laboratorio_05/Underst_cola.c
+++ Laboratorio_05/Underst_cola.c
@@ -11,7 +11,7 @@ struct Queue {
 	int* array;
 };
 
-// Funcion para crear una cola dada una capacidad. Inicia su tamaño de
+// Función para crear una cola dada una capacidad. Inicia su tamaño de
 // cola como 0
 struct Queue* createQueue(int capacity)
 {
@@ -40,32 +40,32 @@ int isEmpty(struct Queue* queue)
 	return (queue->size==0);//yo no lo llamaría size pero weno, es muy confuso, size si va cambiando, es como nuestra flecha, solo que simplemente es un entero. Tiene el valor del número de elementos que tiene nuestra cola
 }
 
-// Funcion para agregar un elemento a la cola.
+// Función para agregar un elemento a la cola.
 // Cambia la parte trasera y el tamano.
 void enqueue(struct Queue* queue, int item)//le estoy enviando la dirección de una estructura cola
 {
 	if(isFull(queue))//si devuelve true/verdadero si entra
 		return;//que curioso regresa, eso me sirve, creo, solo toma en cuenta que es por que la función es void, es bastante inútil(por que ¿porque querrías devolver algo cuando la función no devuelve nada?), pero te evita poner un else
 		//¿por qué no imprime un mensaje de aviso?
-	queue->rear=(queue->rear+1) % queue->capacity;//rear=5%5=0//rear=1%5=1  ah caray!
-	queue->array[queue->rear]=item;//array[0]=21;//array[1]=2;
-	queue->size+=1;//queue->size+1;//size=0+1;//size=2//
+	queue->rear=(queue->rear+1) % queue->capacity;//rear=4+1%5=5%5=0//rear=0+1%5=1%5=1///rear=1+1%5=2  ah caray!//pero no entiendo por qué no poner rear directamente en cero y evitar esta línea
+	queue->array[queue->rear]=item;//array[0]=21;//array[1]=2;//array[2]=56;
+	queue->size+=1;//queue->size+1;//++queue->size;//size=0+1;//size=2//size=3
 	printf("%d agregado a la cola\n", item);
 }
 
-// Funcion para eliminar un elemento de la cola.
+// Función para eliminar un elemento de la cola.
 // Cambia el frente y el tamano.
 int dequeue(struct Queue* queue)
 {
 	if (isEmpty(queue))
 		return 3;//¿por qué no imprime un mensaje de aviso?
-	int item=queue->array[queue->front];//array[0] sabemos que es 21//array[1]=2//
-	queue->front=(queue->front + 1) % queue->capacity;//front=0+1%5=1//front=1+1%5=2;
-	queue->size-=1;//queue->size - 1;//size=1//size=0;es que es cero por que nadamás lo imaginé hasta edad de gatito
+	int item=queue->array[queue->front];//array[0] sabemos que es 21//array[1]=2//array[2]=56;
+	queue->front=(queue->front+1) % queue->capacity;//front=0+1%5=1//front=1+1%5=2;//front=2+1%5=3;
+	queue->size-=1;//queue->size - 1;//size=3//size=2;//size=1
 	return item;
 }
 
-// Funcion para obtener el frente de la cola.
+// Función para obtener el frente de la cola.
 int front(struct Queue* queue)
 {
 	if (isEmpty(queue))
@@ -73,15 +73,15 @@ int front(struct Queue* queue)
 	return queue->array[queue->front];
 }
 
-// Funcion para obtener la parte trasera de la cola
+// Función para obtener la parte trasera de la cola
 int rear(struct Queue* queue)
 {
 	if (isEmpty(queue))
-		return 90;//què regresa?? nada, namás se sale de esta función
+		return 90;//???
 	return queue->array[queue->rear];
 }
 
-// Funcion principal para probar las operaciones.
+// Función principal para probar las operaciones.
 int main()//first in, first out
 {
 	struct Queue* queue=createQueue(5);//nos regresa la colita ya creada
@@ -93,6 +93,15 @@ int main()//first in, first out
     enqueue(queue, 19);
 
 	printf("%d eliminado de la cola\n", dequeue(queue));
+	printf("%d eliminado de la cola\n", dequeue(queue));
+	printf("%d eliminado de la cola\n", dequeue(queue));
+	printf("%d eliminado de la cola\n", dequeue(queue));
+	printf("El elemento de enfrente es: %d\n", front(queue));
+	printf("El elemento de al final es: %d\n", rear(queue));//se me hace curioso que con rear encolemos(lo cual se lleva a cabo en el inicio del arreglo), pero aun así sea capaz de darnos el último elemento
+	
+	enqueue(queue, 21);//esto ya no se podía, como va avanzando hacia enfrente, ya me acabe mi arreglo
+	enqueue(queue, 2);
+	enqueue(queue, 56);
 	printf("El elemento de enfrente es: %d\n", front(queue));
 	printf("El elemento de al final es: %d\n", rear(queue));
 
@@ -100,3 +109,4 @@ int main()//first in, first out
 }
 //nunca liberamos memoria :c. Usamos dos veces malloc()
 //nostros vamos emtiendo por rear, y vamos sacando por front, size es el que se va moviendo
+//is full and is empty utilizan size 
--- Laboratorio_05/Underst_pila.c
+++ Laboratorio_05/Underst_pila.c
@@ -10,7 +10,7 @@ struct Stack
 	int* array;
 };
 
-// Funcion para crear una pila dada una capacidad. Inicia su tamaño de
+// Función para crear una pila dada una capacidad. Inicia su tamaño de
 // pila como 0
 struct Stack* createStack(int capacity)
 {
@@ -33,33 +33,36 @@ int isEmpty(struct Stack* stack)
 	return stack->top==-1;//booleano false or true
 }
 
-// Funcion para agregar un elemento a la pila. Incrementa el tope en 1
+// Función para agregar un elemento a la pila. Incrementa el tope en 1
 void push(struct Stack* stack, int item)
 {
 	if(isFull(stack))
 		return;//por que este puede no regresar nada, pero los otros no
-	stack->array[++stack->top]=item;//aquí si cambia top//array[0]=21;
+	stack->array[++stack->top]=item;//aquí si cambia top//array[0]=21;//esta línea en las notas se describe como: primero se incrementa el índice y en ese índice se guarda el elemento
 	printf("%d push a la pila\n", item);
+	//por cierto, creo que es lo mismo stack->top++
 }
 
-// Funcion para quitar un elemento de la pila. Decrementa el tope en 1
+// Función para quitar un elemento de la pila. Decrementa el tope en 1
 int pop(struct Stack* stack)
 {
 	if(isEmpty(stack))
 		return 20;
-	return stack->array[stack->top--];//cuando quitas, quitas en top(recordando que top es el que se va moviendo)
-	//simplemente perdemos el rastro, no lo eliminamos
+	return stack->array[stack->top--];//top si se modifica//cuando quitas, quitas en top(recordando que top es el que se va moviendo)//esta línea en las notas se describe como: primero se disminuye el índice y se devuelve el valor en ese índice
+	//simplemente perdemos el rastro, no lo eliminamos//pero es díficil de entender, top siempre queda digamos apuntando a un numero extra del que en realidad estamos, al disminuir top apuntamos al último elmento, es por eso que
+	//podemos regresar el elemento que queremos olvidar/elimnar por que la siguiente vez que hagamos push(después de este/un pop) se sobreescribirá
+	//por cierto, creo que es lo mismo --stack->top
 }
 
 // Funcion para regresar el valor del tope de la pila sin quitarlo
-int peek(struct Stack* stack)
+int peek(struct Stack* stack)//en las notas del profesor este es top
 {
 	if(isEmpty(stack))
 		return 22;
 	return stack->array[stack->top];
 }
 
-// Funcion principal para probar las operaciones.
+// Función principal para probar las operaciones.
 int main()
 {
 	struct Stack* stack=createStack(5);
@@ -70,7 +73,7 @@ int main()
     push(stack, 55);
     push(stack, 19);
 
-	printf("%d eliminado de la pila\n", pop(stack));
+	printf("%d eliminado de la pila\n", pop(stack));//no es muy ejemplificador, pero esa función nos regresa un int, 
     printf("El elemento al tope es: %d\n", peek(stack));//te muestra el elemento que esta en el tope
 
 	return 0;
