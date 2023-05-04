6ee151d15cbe5167830fe906a4668b7ec963315d
Sonia Hernandez Fierros <hernandezfierrossonia.2023.1@gmail.com>
2023-05-04 00:23:03 +0000

    práctica_07 intentando

--- Pruebas_P_07/Modf_ListaCircular.c
+++ Pruebas_P_07/Modf_ListaCircular.c
@@ -6,7 +6,8 @@
 struct Node//verás que se vuelve a llamar a sí misma
 {//bro nunca terminare de entender estructuras, me acuerdo que en ajedrez ni podía usarlas normal
     int data;
-    struct Node* next;//esto es una referencia(enlace)/apuntador al siguiente nodo de la lista
+    struct Node* next;//mira el * es por que guarda direcciones, next guar5da direcciones, weno, tecnicamente apunta a nodos, pero los nodos son direcciones, es por eso que es struct Node, por que ese tipo de dato guarda, no por que sea un ciclo
+    //esto es una referencia(enlace)/apuntador al siguiente nodo de la lista
     /*por qué no se hace infinito? 
     Pienso que no es como una función dentro de una función, no creo que esto sea recursividad
     , pero weno es una acción que crea más acciones infinitas*/
@@ -45,12 +46,14 @@ struct Node* addFront(struct Node* last, int data)
     newNode->data=data;
 
     //Se guarda la dirección del primer nodo actual en el nuevo nodo
-    newNode->next=last->next;
+    newNode->next=last->next;//en este caso last->next es un nodo, es fácil saberlo por que los next no apuntan a otros next, apuntan a un nodo
 
-    //Se hace el nuevo nodo como la cabeza
+    //Se hace el nuevo nodo como la ¿cabeza?
     last->next=newNode;
+    
+    //ojo que aquí no se se hace last=newNode; por lo que concluimos que last siempre estará en el último elemento de la derecha
 
-    return last;
+    return last;//supongo que regresa last por que se modificó, y como de este si tenemos que tener rastro (no como los newNodes por que esos se conectan pero se pierden)
 }
 
 //Agregar nodo al final
@@ -67,7 +70,9 @@ struct Node* addEnd(struct Node* last, int data)
     newNode->data=data;
 
     //Se almacena la direción del nodo cabeza al siguiente del nuevo nodo
-    newNode->next=last->next;//es por que agregamos al final//con está línea concluimos que last siempre va a estar al final/derecha, a diferencia de head
+    newNode->next=last->next;/*rear y front es como lo mismo, mi pregunta es ¿esta bien decir que en este programa la lista circular se comporta como una pila?, es que insertamos donde este last. Respuesta: No,
+    no esta bien decir eso, por que last es nuestra referencia, con last nos ubicamos, por eso la usamos en todo tipo de add's, pero en realidad para front y end insertra no es exactamente "donde este last", 
+    por que insertamos antes(addFront) y después(addEnd)*/
 
     //Se apunta al actual último nodo al nuevo nodo
     last->next=newNode;//yo sé que ya lo sabes, pero los next apuntan a nodos, aunque cuando veas holis->next->next ¿es un next que apunta a otro next? no, yo no lo veo así, yo lo veo como el next del (next de holis)
@@ -75,7 +80,7 @@ struct Node* addEnd(struct Node* last, int data)
     //Se hace al nuevo nodo como el último nodo
     last=newNode;
 
-    return last;//aquí si entiendo que regresas al nuevo last
+    return last;//aquí si entiendo que regresas al nuevo last, no bro, last significa último en entrar, no es como rear
 }
 
 //Insertar nodo después de un nodo específico
@@ -84,13 +89,15 @@ struct Node* addAfter(struct Node* last, int data, int item)
     //Se revisa si la lista está vacía
     if(last==NULL) return NULL;//mira esto es muy útil cuando quieres terminar una función, pero no puedes devolver algo válido
 
-    struct Node *newNode, *p; 
+    struct Node *newNode, *p;//en lugar d ep podías poner temp
 
-    p=last->next;
+    p=last->next;//es decir el primer nodo. por que last es como rear (confirmado)
+    //last->next es un nodo, para que algo sea igual a ese nodo tiene que ser struct Node*
 
-    do{
-    //Si se encontró el elemento, se coloca el nuevo nodo después de él
-        if(p->data==item) 
+    do
+    {
+        if(p->data==item)//Si se encontró el elemento, se coloca el nuevo nodo después de él
+        //p->data es un entero. Empezamos por el primero
         {
             //Se asigna memoria al nuevo nodo
             newNode=(struct Node*)malloc(sizeof(struct Node));
@@ -99,18 +106,19 @@ struct Node* addAfter(struct Node* last, int data, int item)
             newNode->data=data;
 
             //Se hace como siguiente el nodo actual siguiente del nuevo nodo
-            newNode->next=p->next;
+            newNode->next=p->next;//p->next es un nodo//curioso es que esta línea también funciona por si p es el último nodo
             
             //Se pone al nuevo nodo como el siguiente de p
-            p->next=newNode;
+            p->next=newNode;//los next siempre apuntan a un nodo o a NULL
 
             //Si p es el último nodo, se hace al nuevo nodo como el último nodo
             if(p==last) last=newNode;
-            return last;
+            return last;//ojo, con que entre una vez a este if y nos regresamos
         }
 
-        p=p->next;
-    } while(p!=last->next);
+        p=p->next;//como empezamos en el primer nodo vamos avanzando hacia allá ->
+    } while(p!=last->next);//esta condición es pa' que no demos la vuelta, pa' que no repitamos pues
+    //p y last->next son enteros
 
     printf("\nEl nodo dado no esta presente en la lista.");
     return last;
@@ -118,75 +126,85 @@ struct Node* addAfter(struct Node* last, int data, int item)
 }
 
 //Borrar un nodo
-void deleteNode(struct Node** last, int key)//es el único que tiene un **
+void deleteNode(struct Node** lasti, int key)/*es el único que tiene un **, es que le envié &last, el cual es la dirección de un apuntador,
+es como escirbir struct Node** lasti=&last; */
+//mira manta ya investigué y es lo mismo *lasti que en otras funciones es last, solo que el profesor nos quería hacer sufrir, si no mre crees revisa el documento apuntadores_entendiendolos_con_struct_y_memDinamic
 {
     //Si la lista ligada está vacía
-    if(*last==NULL) return;
+    if(*lasti==NULL) return;//es lo mismo que last==NULL en main y en las otras funciones
 
     //Si la lista contiene solo un nodo
-    if((*last)->data==key &&(*last)->next==*last) 
+    if((*lasti)->data==key && (*lasti)->next==(*lasti))/*utilizar *lasti en esta función(ya sé que es struct Node**lasti) equivale a usar last en main, 
+    last en main es una dirección*/
+    //brother es muy importante poner los paréntesis de (*lasti)->, por que como que se equivoca si pones *lasti->, por cierto (*lasti) es lo mismo que last en otras funciones
     {
-        free(*last);
-        *last=NULL;
+        free(*lasti);//mira de algo estoy segura, con esto eliminamos el cuadro que viene siendo el nodo, lo que viene siendo data y next
+        //es como hacer en el main free(last), sin embargo last y *last no es lo mismo, por lo que este free(una dirección)
+        *lasti=NULL;//no sé pero si no haces *lasti=NULL queda valiendo la dirección que nos dio la memoria dinámica, no entiendo por qué, es que *lasti (o last en main) valen un nodo, así que si no tiene un nodo al qué señalar el last, entonces =NULL tal como en el main
         return;//por que no puede devolver algo
     }
 
-    struct Node *temp=*last, *d;
+    struct Node *temp=*lasti;//tu ve ese *lasti como last en cualquier función incluyendo main
+    struct Node *d;
 
     //Si se va a eliminar el último
-    if((*last)->data==key)
+    if((*lasti)->data==key)
     {
         //Encontrar el nodo antes del último nodo
-        while(temp->next != *last) temp=temp->next;
+        while(temp->next != *lasti) temp=temp->next;//eso jeje, encuentra el anterior a last o *lasti, sin modificar last o lo que e slo mismo *lasti
 
-        //Apuntar un nodo temporal al siguiente del último es decir el primer nodo
-        temp->next=(*last)->next;
-        free(*last);
-        *last=temp->next;
+        //Apuntar un nodo temporal al siguiente del último, es decir el primer nodo
+        temp->next=(*lasti)->next;
+        free(*lasti);//mira de algo estoy segura, con esto eliminamos el cuadro que viene siendo el nodo, lo que viene siendo data y next
+        *lasti=temp->next;//ah caray, por que cambiamos a last al frente, si de hecho eso pasa
+        //se me hace bien curioso que a pesar de haber liberado *lasti, aun podamos ocuparlo, creo que nunca voy a entender free()
+        //temp->next es un nodo
     }
 
     //Recorrido al nodo que va a ser eliminado
-    while(temp->next!=*last && temp->next->data !=key)
-    {
-        temp=temp->next;
-    }
+    while(temp->next!=(*lasti) && temp->next->data!=key) temp=temp->next;
+    //iniciamos con que temp=last así que esta parte temp->next!=lasti, es para que no completemos una vuelta
+    //y esta parte temp->next->data!=key yo entiendo que es para que quede de forma que el siguiente a temp sea key
 
     //Si el nodo a eliminar se encuentra
-    if(temp->next->data==key)
+    if(temp->next->data==key)//por que no sabemos si lo encontró
     {
         d=temp->next;
         temp->next=d->next;
-        free(d);
+        free(d);//nunca entenderé free(), y si lo entiendo debería ser posibloe asignarle un valor a d, pero ahora que lo veo d no es memora dinámica
     }
 }
 
 //creo que pa' imprimir la lista
-void traverse(struct Node* last) 
+void traverse(struct Node* last)//aquí recibo la dirección del nodo que guarda last
 {
     struct Node* p;
 
-    if(last==NULL)
+    if(last==NULL)//las es una dirección, como nota NULL y direcciones tienen una bella amistad
     {
         printf("La lista está vacía");
         return;//por que no puede devolver algo
     }
 
-    p=last->next;
+    p=last->next;//last->next es un nodo y es el primero de hecho, siempre es el primero.
 
-    do{
-        printf("%d", p->data);
+    do
+    {
+        printf("%d ", p->data);
         p=p->next;
 
-    } while(p!=last->next);
+    } while(p!=last->next);//p y last->next son nodos, esta línea evita que empecemos de nuevo, es decir que demos la vuelta
+    printf("\n");
 }
 
 int main() 
 {
-    struct Node* last=NULL;/*esta línea es crear un apuntador que guarda la dirección de un tipo de dato como struct Node
+    struct Node* last;/*=NULL;esta línea es crear un apuntador que guarda la dirección de un tipo de dato como struct Node
     Lo que se me hace curioso es que no quiero la dirección de la variable, por algo la estoy modificando a NULL*/
     //Voy descubriendo que no hay función para crear un nodo
     //a este nodo podiamos haberle creado su memoria aquí, ¿te preguntabas por que era pointer? por que es memoria dinámica, las variables son pointer
-    //segpun yo era lo mismo que last=NULL;
+    //segpun yo era lo mismo que 
+    last=NULL;
 
     /*muy interesante a last no se le crea memoria dinámica, el addToEmpty le crea memoria dinámica al newNode, pero es
     curioso que cuando last=newNode en esa función pero a last no le creamos memoria dinámica*/
@@ -203,21 +221,32 @@ int main()
     last=addEnd(last, 22);
     last=addEnd(last, 3);
 
-    last=addFront(last, 19);
+    last=addFront(last, 19);//recibimos una copia del last actualizado (con lo que le hicimos dentro de la función) y la hacemos la original(con el igual)
     last=addFront(last, 55);
     last=addFront(last, 56);
 
     last=addAfter(last, 10, 2);
     last=addAfter(last, 17, 3);//el item es lo que tiene que buscar, no es una posición, supongo que es lógico pues no es un arreglo
-    last=addAfter(last, 1, 2);
+    last=addAfter(last, 1, 2);/*supongo que siempre tengo que devolver last actualizado y no devulevo los newNode's por que como que con after voy a acceder a cualquier cosa
+    , pero todas estas funciones me regresan una copia de last?..., ¿que es exactamente esa copia?, entender cómo fue creado last es muy díficil, por que a todas las funciones le
+    estoy mandando la dirección que guarda last, en un principio esa dirección era NULL, pero luego en addToEmpty me dieron la dirección de un nodo, 
+    desde entonces last vale la dirección de los nodos(entonces last noe s exactamente un nodo, ni parecido...)*/
 
     traverse(last);
 
-    deleteNode(&last, 8);//enviamos la dirección del apuntador
+    deleteNode(&last, 20);//enviamos la dirección del apuntador
+    //deleteNode elimina en base a buscar un número entero en tu lista
+
+    deleteNode(&last, 17);
+
+    traverse(last);
+
+    deleteNode(&last, 55);
 
     traverse(last);
 
     return 0;
 }
-//antes no entendía head, ahora no entiendo last
+//antes no entendía head, ahora no entiendo last. last guarda direcciones, es un apuntador, no es un nodo, guarda la dirección del último nodo, ese nodo como todos se llama newNode, solo que tiene un bicho pegado a él que absorbe su dirección y se llama last
 //todos los nodos creados se llaman newNode, hasta los que llegan a ser last, pero no nos importa su nombre por que no los utilizamos por su nombre si no por enlaces(next) de los nodos(empezando por last)
+//a pesar de que es lista circular, al imprimirlo no se ve como circulo, con esto quiero decir que hay front y rear/end
